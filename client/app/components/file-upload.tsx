820deac3797bdf9ea5fc67dce40282a4c7081d9e
Nirmal Singh <nirmalsingh100754@hotmail.com>
2026-04-12 03:38:27 +0530

    feat: implement file upload functionality in FileUploadComponent

--- client/app/components/file-upload.tsx
+++ client/app/components/file-upload.tsx
@@ -2,9 +2,21 @@
 import { Upload } from "lucide-react";
 
 const FileUploadComponent = () => {
+    const handleFileUploadClick = () => {
+        const el=document.createElement("input");
+        el.setAttribute("type","file");
+        el.setAttribute("accept",".pdf");
+        el.addEventListener("change",(e)=>{
+            if(el.files && el.files.length>0)
+            {
+                const file=el.files.item(0);
+            }
+        });
+        el.click();
+    };
   return (
     <div className="w-full max-w-sm rounded-[32px] border border-slate-700 bg-gradient-to-br from-slate-900 via-slate-950 to-slate-900 p-8 shadow-2xl shadow-slate-950/40">
-      <div className="flex flex-col justify-center items-center gap-4 text-center text-slate-100">
+      <div onClick={handleFileUploadClick} className="flex flex-col justify-center items-center gap-4 text-center text-slate-100">
         <div className="flex h-20 w-20 items-center justify-center rounded-3xl bg-slate-800/90 border border-slate-700">
           <Upload className="h-10 w-10 text-indigo-400" />
         </div>
