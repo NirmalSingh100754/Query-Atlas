f0221ce67b0a44f6d2982ea3bd9c71e388ef476d
Nirmal Singh <nirmalsingh100754@hotmail.com>
2026-04-12 03:09:46 +0530

    feat(client): add initial configuration and assets
    - Create pnpm workspace configuration to ignore specific built dependencies.
    - Add PostCSS configuration for Tailwind CSS integration.
    - Include various SVG assets for UI components (file, globe, next, vercel, window).
    - Establish TypeScript configuration for the client with strict type checking and module resolution.

---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-screen flex">
+      <div className="w-[50vw] min-h-screen border-red-600">1</div>
+      <div className="w-[50vw] min-h h-screen border-l-2">2</div>
+    </div>
+  );
+}
