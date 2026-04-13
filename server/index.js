bb489e6a4453d2c93648cd5d3d1f1cebba17b9b3
NirmalSingh100754 <nirmalsingh100754@gmail.com>
2026-04-13 15:16:39 +0530

    feat: enhance file upload functionality with multer and improve response messages

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 import cors from "cors";
 import express from "express";
+import multer from "multer";
+import { stat } from "node:fs";
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.filename + '-' + uniqueSuffix)
+  }
+})
+
+
+const upload = multer({ dest: 'uploads/' });
 const app = express();
 
 app.use(cors());
@@ -8,6 +22,14 @@ app.use(cors());
 app.get("/", (req, res) => {
   return res.json({
     message: "Hello from the server",
+    status: "success",
+  });
+});
+
+app.post("/upload",upload.single("pdf"), (req, res) => {
+  return res.json({
+    message: "File uploaded successfully",
+    status: "success",
   });
 });
 
